9fb9f2dbb3f2652c26f51abdd6829ab2b48770f1
Mujeeb4582 <mujeeburahman4582@gmail.com>
2024-09-19 11:33:59 +0500

    add the all files

---
+++ sum-all-odd-fibonacci-numbers.js
@@ -0,0 +1,6 @@
+module.exports = function (num) {
+  // first get the fibonacci squence using for loop
+  // then take out odd numbers from series
+  // sum the odd numbers
+  // write your code here below
+};
